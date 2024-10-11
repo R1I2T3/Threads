5cd97b1c5fbe52c3669801992b78e6584b461637
Ritesh jha <jhar27018@gmail.com>
2024-10-11 09:26:16 +0530

    dist folder added

--- threads_server/src/index.js
+++ threads_server/src/index.js
@@ -5,7 +5,11 @@ import cookieParser from "cookie-parser";
 import UserRouter from "./routes/user.route.js";
 import PostRouter from "./routes/post.route.js";
 import { v2 as cloudinary } from "cloudinary";
+import { join } from "path";
+import { fileURLToPath } from "url";
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = join(__filename, "..");
 dotenv.config();
 
 const port = process.env.PORT || 3000;
@@ -14,12 +18,13 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  return res.json({ message: "Hello world" });
-});
+app.use(express.static(join(__dirname, "dist")));
 
 app.use("/api/user", UserRouter);
 app.use("/api/post", PostRouter);
+app.get("*", (req, res) => {
+  res.sendFile(join(__dirname, "dist", "index.html"));
+});
 connectDB()
   .then(
     app.listen(port, () => {
